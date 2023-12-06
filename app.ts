f21344afc0cbab9a06183617925017c00c8e635c
Victor Martinez <vicmtzdev@gmail.com>
2023-12-05 19:22:53 -0600

    TS - Tarea 1

--- app.ts
+++ app.ts
@@ -1,11 +1,46 @@
-
-//Función anomina autoinvocada
-
 (() => {
-    
-    const a: number = 10;
-    
-    console.log(a);
+
+  // Tipos
+  const batman: string = 'Bruce';
+  const superman: string = 'Clark';
+
+  const existe: boolean = false;
+
+  // Tuplas
+  const parejaHeroes: [string, string] = [batman, superman];
+  const villano: [string, number, boolean] = ['Lex Lutor', 5, true];
+
+  // Arreglos
+  const aliados: string[] = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+
+  //Enumeraciones
+
+  enum HeroForce {
+    acuaman = 0,
+    batman = 1,
+    flash = 5,
+    superman = 100,
+  }
+
+  const fuerzaFlash: HeroForce = HeroForce.flash;
+  const fuerzaSuperman: HeroForce = HeroForce.superman;
+  const fuerzaBatman: HeroForce = HeroForce.batman;
+  const fuerzaAcuaman: HeroForce = HeroForce.acuaman;
+
+  // Retorno de funciones
+  function activar_batiseñal(): string {
+    return 'activada';
+  }
+
+  function pedir_ayuda(): void {
+    console.log('Auxilio!!!');
+  }
+
+  // Aserciones de Tipo
+  const poder: any = '100';
+  const largoDelPoder: number = (poder as string).length;
+  console.log(largoDelPoder);
+
 
 })()
 
