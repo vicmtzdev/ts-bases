217389dc453522bf9230b4a67620bc1a43aa40a1
Victor Martinez <vicmtzdev@gmail.com>
2023-12-20 18:22:19 -0600

    TS - namespaces

--- main.js
+++ main.js
@@ -1,61 +1,12 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Super velocidad', 'Viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Super fuerza', 'Volar'],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return this.name + '' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Victor',
-        age: 25,
-        address: {
-            id: 125,
-            zip: 'KY13',
-            city: 'New York',
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Alejandra',
-        age: 24,
-        address: {
-            id: 123,
-            zip: 'KY13',
-            city: 'New York',
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFuntion;
-    addNumbersFuntion = (a, b) => {
-        return a + b;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Victor'));
 //# sourceMappingURL=main.js.map
